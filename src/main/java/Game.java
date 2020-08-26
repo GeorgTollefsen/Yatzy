04b0534284995977eccdcab9ef022d9eea3df8b8
Lars Magelssen <lars.magelssen@gmail.com>
2020-08-26 15:06:01 +0200

    lagde en generell funksjon for å telle tall

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,34 +1,31 @@
 public class Game {
 
     public static int makeNumber(int[] tall) {
-        int counter = 0;
-        for(int i=0; i<tall.length; i++){
-        if(tall[i]==1){
-            counter++;
-        }
-        }
-        return counter;
+        return countThis(tall, 1);
     }
 
     public static int checkForTwos(int[] twos) {
-        int counter = 0;
-        for (int i=0;i<twos.length;i++) {
-            if(twos[i]==2) {
-                counter += 1;
-            }
-        }
-        return counter;
+        return countThis(twos, 2);
     }
 
 
     public static int countThrees(int[] tall) {
+        return countThis(tall, 3);
+    }
+
+    public static int countFours(int[] fours) {
+        return countThis(fours, 4);
+
+    }
+
+
+    public static int countThis(int[] kast, int checkNumber) {
         int counter = 0;
-        for(int t : tall){
-            if(t == 3){
+        for (int number : kast) {
+            if (number == checkNumber) {
                 counter++;
             }
         }
         return counter;
     }
-
 }
