a71689b561c7f2a35cde188aa8e87273f4ab0328
Georg Tollefsen <georgto@me.com>
2020-08-26 14:33:12 +0200

    fikset

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,6 +1,12 @@
 public class Game {
 
-    public static int makeNumber(int tall) {
-        return tall;
+    public static int makeNumber(int[] tall) {
+        int counter = 0;
+        for(int i=0; i<tall.length; i++){
+        if(tall[i]==1){
+            counter++;
+        }
+        }
+        return counter;
     }
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertEquals;
 public class GameTest {
     @Test
     public void isOne() {
-        assertEquals(1, Game.makeNumber(1));
+        assertEquals(1, Game.makeNumber(new int[] {1,2,3,4,5}));
     }
 
 }
